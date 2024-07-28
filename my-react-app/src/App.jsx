b6e5bfb50614cb89817ba71f9589e3b914ed8670
eedu7 <mueedahmad067@gmail.com>
2024-07-28 14:56:48 +0500

    Rendering List and Obj

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,20 +3,25 @@
 // import Student from "./student-component-props/student.jsx"
 // import UserGreeting from "./condition-rendering-component/userGreeting.jsx";
 
+import List from "./renderingList/List.jsx";
+
 function App() {
     return (<>
-            {/*  Card Component  */}
-            {/*<Card/>*/}
+        {/*  Card Component  */}
+        {/*<Card/>*/}
 
-            {/* Button Component */}
-            {/*<Button/>*/}
+        {/* Button Component */}
+        {/*<Button/>*/}
 
-            {/* Student Component with Props*/}
-            {/*<Student id={1} name="Son Goku" age={42} student={false}/ >*/}
+        {/* Student Component with Props*/}
+        {/*<Student id={1} name="Son Goku" age={42} student={false}/ >*/}
 
-            {/* Conditional Rendering UserGreeting*/}
-            {/*<UserGreeting isLoggedIn={false} username="MueedAhmad"/>*/}
-        </>)
+        {/* Conditional Rendering UserGreeting*/}
+        {/*<UserGreeting isLoggedIn={false} username="MueedAhmad"/>*/}
+
+        {/* Rendering List*/}
+        <List/>
+    </>)
 }
 
 export default App;
---
+++ my-react-app/src/renderingList/List.jsx
@@ -0,0 +1,41 @@
+function List() {
+    // List of items
+    const fruits = ["apple", "banana", "pine-apple", "orange"];
+
+    // List of objects
+    const fruitsObj = [
+        {"id": 1, "name": "apple", "calories": 95},
+        {"id": 2, "name": "orange", "calories": 45},
+        {"id": 3, "name": "banana", "calories": 105},
+        {"id": 4, "name": "coconut", "calories": 159},
+        {"id": 5, "name": "pineapple", "calories": 37},
+    ]
+
+    // fruitsObj.sort((a, b) => a.name.localeCompare(b.name));  // Ascending Order Alphabetical Order
+    // fruitsObj.sort((a, b) => b.name.localeCompare(a.name));  // Descending Order Alphabetical Order
+    // fruitsObj.sort((a, b) => a.calories - b.calories);  // Ascending Order Numerical
+    // fruitsObj.sort((a, b) => b.calories - a.calories);  // Desending Order Numerical
+
+    // Working wit the list
+    // const listItem = fruits.map(fruit => <li>{fruit}</li>)
+
+    // Working with the list of objects
+    const listFruitObj = fruitsObj.map(fruit =>
+        <li key={fruit.id}>{fruit.name}: &nbsp;{fruit.calories}</li>)
+
+
+    return (
+        // Rendering the list
+        // <ol className="list">
+        //     {listItem}
+        // </ol>
+
+        <>
+            <ul>
+                {listFruitObj}
+            </ul>
+        </>
+    );
+}
+
+export default List;
